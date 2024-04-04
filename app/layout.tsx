6f53b79515dfc5ed7ec78ea5f6df43eb18c9c93f
binyul <jiibinyul@gmail.com>
2024-04-04 17:01:36 +0545

    footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import footer from "@/components/footer";
 
 const poppins = Poppins({
   weight: "400",
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html className={poppins.className}>
       <body>
         <main>{children}</main>
+        <footer/>
       </body>
     </html>
   );

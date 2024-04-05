91eb1b64857fb475fa4b3aeb211cb5f75d9b2764
Amos-Bista <120589997+Amos-Bista@users.noreply.github.com>
2024-04-05 12:23:25 +0545

    created-navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="h-32 bg-gray-900/80 px-6 relative ">
+      <img src="/dataspacelogo1.png" alt="" className="h-16 absolute top-5" />
+      <div className="flex justify-end">
+        <ul className="flex gap-12 absolute top-12 right-32 text-white font-light text-xl ">
+          <li>Home</li>
+          <li>Service</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
 
-export default NavBar
+        <img
+          src="/NavImg.jpeg"
+          alt=""
+          className="h-12 w-12 rounded-full absolute top-9 right-12"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;

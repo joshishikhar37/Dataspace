7fc27786e395153ab2c9217be4e76e408e8a36ce
binyul <jiibinyul@gmail.com>
2024-04-07 14:53:56 +0545

    fully completed

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -8,8 +8,8 @@ const Plans = () => {
     height: "113.34px"
     }
   return (
-    <div className='shadow-2xl rounded-lg w-[300px] ml-16 my-10 rounded-lg '>
-        <div className='relative'>
+    <div className='shadow-2xl rounded-lg w-[300px] ml-16 my-10 rounded-lg'>
+        <div className='relative '>
           <img src= "/plans.png" alt="footerimage" style={imagestyles} className='rounded-t-lg' />
           <h1 className='absolute top-[70%] left-10 text-white font-9xl font-black'>Virtual Private Server Bronze</h1>
         </div>
@@ -40,7 +40,7 @@ const Plans = () => {
             </div>
             
           </div >
-          <div className='flex justify-center '>
+          <div className='flex justify-center hover:scale-105'>
             <Buttoncart/>
           </div>
         </div>

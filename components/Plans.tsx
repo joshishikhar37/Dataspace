6db35dbdad54a8ff81b4565cee207273aaa8ccd6
binyul <jiibinyul@gmail.com>
2024-04-07 13:53:03 +0545

    cartsection

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -1,19 +1,50 @@
 import React from 'react'
+import Buttoncart from "@/components/buttoncart"
 
 const Plans = () => {
   const imagestyles ={
-    width: "304px",
+    width: "308px",
     height: "113.34px"
     }
   return (
-    <div className='bg-white border-2 border-indigo-600 rounded-lg w-[308px] h-[420px] ml-16 my-10'>
-      <div>
-          <img src= "/plans.png" alt="footerimage" style={imagestyles} className='absolute' />
-          <h1 className='relative text-white top-[84px] left-[38px]'>Virtual Private Server Bronze</h1>
-      </div>
-      <div></div><div></div>
-      <div></div>
+    <div className='shadow-2xl rounded-lg w-[308px] h-[420px] ml-16 my-10 rounded-lg '>
+        <div className='relative'>
+          <img src= "/plans.png" alt="footerimage" style={imagestyles} className='rounded-t-lg' />
+          <h1 className='absolute top-[70%] left-10 text-white font-9xl font-black'>Virtual Private Server Bronze</h1>
+        </div>
         
+        <div className='h-[72.7%]'>
+          <div className='h-[80%] text-black flex flex-col gap-1 py-5'>
+            <div className='flex mx-auto '>
+                CORE CPU
+            </div>
+            <div className='flex mx-auto '>
+              1 GB RAM
+            </div>
+            <div className=' flex mx-auto'>
+              20 GB DISK SPACE
+            </div>
+            <div className=' flex mx-auto'>
+              1 PUBLIC IP ADDRESS
+            </div>
+            <div className='flex mx-auto '>
+            1 PUBLIC IP ADDRESS
+            </div>
+            <div className='flex mx-auto font-black'>
+                Monthly
+            </div>
+            <div className='flex mx-auto '>
+
+               Nrs.<p className='font-black'>1,000.00</p> 
+            </div>
+            
+          </div >
+          <div className='h-[20%] '>
+            <Buttoncart/>
+          </div>
+        </div>
+     
+   
     </div>
   )
 }

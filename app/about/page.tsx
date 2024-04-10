e0fa808cceeb03b7c3893b4f296d2a47d7359777
binyul <jiibinyul@gmail.com>
2024-04-10 15:39:35 +0545

    milestone added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Aboutcard from "@/components/aboutcard";
 import Image from "next/image";
 import aboutData from "@/assets/aboutData.json";
+import Milestone from "@/components/milestone";
 type aboutData = {
   [key: string]: {
     title: string;
@@ -13,12 +14,13 @@ type aboutData = {
 };
 const About = () => {
   const aboutDataTyped = aboutData as aboutData;
-  const chunkedKeys = chunkArray(Object.keys(aboutData), 2);
+  
+  const chunkedKeys:string[][] = chunkArray(Object.keys(aboutData), 2);
   return (
     <main>
       <Abouthero />
 
-      <section className="flex-col justify-center">
+      <section className="flex-col justify-center py-16">
         <h1 className="flex justify-center text-3xl py-6">WHY DATASPACE?</h1>
         <Image
           src="/aboutbody.png"
@@ -28,27 +30,38 @@ const About = () => {
           className="mx-auto py-4"
         />
       </section>
-      
+
       <div className="">
-      {/* Map through the chunked keys */}
-      {chunkedKeys.map((chunk, index) => (
-        <div key={index} className="flex justify-center">
-          {/* Map through each pair of keys in the chunk */}
-          {chunk.map((key:string) => (
-            <Aboutcard
-              key={key}
-              title={aboutDataTyped[key][0].title}
-              description={aboutDataTyped[key][0].description}
-              image={aboutDataTyped[key][0].image}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
+        {/* Map through the chunked keys */}
+        {chunkedKeys.map((chunk, index) => (
+          <div key={index} className="flex justify-center">
+            {/* Map through each pair of keys in the chunk */}
+            {chunk.map((key: string) => (
+              <Aboutcard
+                key={key}
+                title={aboutDataTyped[key][0].title}
+                description={aboutDataTyped[key][0].description}
+                image={aboutDataTyped[key][0].image}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center text-4xl py-6 pt-5 font-bold ">
+        Our Milestone
+      </div>
+        
+          <div className="flex flex-col">
+            <div className="flex justify-center gap-96 bg-slate-200">
+              <Milestone desc="Years of experience" limit={21} />
+              <Milestone desc="Satisfied Clients" limit={500} />
+              <Milestone desc="Valued Partner" limit={12} />
+            </div>
+          </div>
     </main>
   );
 };
-function chunkArray(array: string | any[], size: number) {
+function chunkArray(array: string[], size: number) {
   const chunkedArr = [];
   let index = 0;
   while (index < array.length) {
